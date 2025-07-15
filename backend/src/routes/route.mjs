b68910a0ccaaf8749e496af5fc42962da5464909
MuthuYogesh <muthuyogeshb@gmail.com>
2025-07-16 01:08:07 +0530

    DevGluck Backend Setup

--- backend/src/routes/route.mjs
+++ backend/src/routes/route.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
 import { createUser } from '../controllers/userController.mjs';
+import { authentication, authorization } from "../auth/authentication.mjs";
 
 const router = express.Router();
 
 // API's here
+router.get('/',authentication, /*API function here*/);
 router.post('/register', createUser);
+router.put("/update/:userid", authentication, authorization, /* API function here */);
 
 export default router;
